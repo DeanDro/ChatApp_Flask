c0de7ed5a1c46499298f262e701e4efebb4d2940
Konstantinos Drosos <dean.drossos@gmail.com>
2020-10-10 16:10:17 -0400

    Creating sockets and chat room page

--- chatApp/src/chatApp.py
+++ chatApp/src/chatApp.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, session
 
+from flask_socketio import SocketIO
+
 
 app = Flask(__name__)
 
 app.secret_key = 'thesuperultimatesecretkey'
+
+socketio = SocketIO(app)
 
 app.config['dbconfig'] = {'host': '127.0.0.1',
                           'user': 'mainUser',
@@ -20,8 +24,18 @@ def home_page():
 
 @app.route('/chatrooms', methods=['GET', 'POST'])
 def chatrooms():
-    return "Loged In!"
+    return render_template('frontend/chatWindow.html')
+
+
+def ack():
+    print('message received')
+
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received json:' +str(json))
+    socketio.emit('Connected', broadcast=True, callback=ack)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
